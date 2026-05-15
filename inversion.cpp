f5a1b6bb0e790f2ba936de624ebb92ecec140b42
ollie-m0 <olliemiller500@protonmail.com>
2026-05-15 17:57:00 +0100

    note: mat_multiply doesn't work properly yet

--- inversion.cpp
+++ inversion.cpp
@@ -3,10 +3,11 @@
 #include <cmath>
 #include <string>
 
-void print_array(double* arr, int n, std::string title="", int setw_size=10)
+void print_array(double* arr, int n, int m, std::string title="", int setw_size=10)
+    // An immensely helpful function for bug fixing.
+    // Prints an n by m matrix to the terminal.
 {
-    // An immensely helpful function for bug fixing
-    for (int i=0; i<(setw_size+1)*n; i++)
+    for (int i=0; i<(setw_size+1)*m; i++)
         std::cout << "_";
     std::cout << "__" << std::endl;
     std::cout << title << std::endl;
@@ -14,16 +15,18 @@ void print_array(double* arr, int n, std::string title="", int setw_size=10)
     for (int i=0; i<n; i++)
     {
         std::cout << '[';
-        for (int j=0; j<n; j++)
+        for (int j=0; j<m; j++)
         {
             std::cout << ' ' << std::setw(setw_size) << *(arr + i*n + j);
-            if (j==n-1) { std::cout << ']' << std::endl; }
+            if (j==m-1) { std::cout << ']' << std::endl; }
         }
     }
-    for (int i=0; i<(setw_size+1)*n; i++)
+    for (int i=0; i<(setw_size+1)*m; i++)
         std::cout << "_";
     std::cout << "__" << std::endl;
 }
+
+
 
 void change_zero_row(double* arr, double* inv_arr, int n, int row, int column, double epsilon=1e-9)
 {
@@ -52,8 +55,8 @@ void change_zero_row(double* arr, double* inv_arr, int n, int row, int column, d
 
 
 double determinant(double* arr_ptr, int n, double epsilon=1e-9)
-{
     // This function calculates the determinant of the provided matrix
+{
     double det;
 
     switch (n)
@@ -87,45 +90,36 @@ double determinant(double* arr_ptr, int n, double epsilon=1e-9)
                 det += determinant(sub_arr, n-1) * *(arr_ptr + k) * pow(-1, k);
             }
     }
-    // if (n==1)
-    // {
-    //     det = *(arr_ptr);
-    // }
-    // else if (n==2)
-    // {
-    //     det = *(arr_ptr) * *(arr_ptr + 3) - *(arr_ptr + 1) * *(arr_ptr + 2);
-    // }
-    // else
-    // {
-    //     det = 0;
-    //     double sub_arr[(n-1) * (n-1)];
-    //     int m;
-    //     for (int k=0; k<n; k++)
-    //     {
-    //         for (int i=1; i<n; i++)
-    //         {
-    //             for (int j=0; j<n; j++)
-    //             {
-    //                 if      (j==k) { continue; }
-    //                 else if (j>k)  { m = j-1;  }
-    //                 else           { m = j;    }
-    //
-    //                 sub_arr[(i-1)*(n-1) + m] = *(arr_ptr + i*n + j);
-    //                 // det += determinant(sub_arr, n-1) * arr[n*(i+1)];
-    //             }
-    //         }
-    //         det += determinant(sub_arr, n-1) * *(arr_ptr + k) * pow(-1, k);
-    //     }
-    // }
     return det;
 }
 
+void mat_multiply(double* arr_1_ptr, double* arr_2_ptr, int n_1, int m_1, int n_2, int m_2, double* product)
+    // Calculate the matrix multiplication arr_1 * arr_2
+    // where arr_1 is an n_1 by m_1 matrix and arr_2 is an n_2 by m_2 matrix.
+{
+    if (m_1 != n_2)
+    {
+        std::cout << "Matricies are not multipliable, as they have incompatible dimensions. Aborting process." << std::endl;
+        std::abort();
+    }
 
+    for (int k=0; k<n_1; k++)
+    {
+        for (int j=0; j<m_2; j++)
+        {
+            *(product + k*m_2 + j) = 0;
+            for (int i=0; i<m_1; i++)
+            {
+                *(product + k*m_2 + j) += *(arr_1_ptr + k*m_1 + i) * *(arr_2_ptr + i*m_2 + j);
+            }
+        }
+    }
+    print_array(product, n_1, m_2, "product");
+}
 
 void gaussian_elimination(double* arr_ptr, double* inv_arr_return, int n, double epsilon=1e-9)
-{
     // This function calculates the inverse of the provided matrix through Gaussian elimination
-
+{
     // Define the identity matrix, which will be returned as the inverted matrix at the end.
     double inv_arr[n*n];
     inv_arr_return = inv_arr;
@@ -209,6 +203,6 @@ void gaussian_elimination(double* arr_ptr, double* inv_arr_return, int n, double
 
     // Print the matrix (only for small n)
     if (n<=50)
-    { print_array(&inv_arr[0], n, "Inverted matrix:", 10); }
+    { print_array(&inv_arr[0], n, n, "Inverted matrix:", 10); }
 
 }
--- main.cpp
+++ main.cpp
@@ -3,16 +3,30 @@
 
 int main()
 {
-    int n = 4;
+    int n = 3;
     double* arr_ptr;
-    double arr[n*n] = {  1,  3,  4,  3,
-                        -1, -3,  7,  8,
-                         5,  6,  6,  4,
-                         1,  2,  2,  6
+    double arr[n*n] = {  1,  3,  4, // 3,
+                        -1, -3,  7, // 8,
+                         5,  6,  6 // 4,
+                         //1,  2,  2,  6
                        };
     arr_ptr = arr;
 
-    double inverse[n*n];
+    double* arr_2_ptr;
+    double arr_2[4*2] = { 5, 4,
+                          3, 7,
+                          // 4, 3,
+                          8, 0 };
+
+    arr_2_ptr = arr_2;
+
+    double  product[3*2];
+    double* product_ptr;
+    product_ptr = product;
+
+    mat_multiply(arr_ptr, arr_2_ptr, 3, 3, 3, 2, product_ptr);
+
+    double  inverse[n*n];
     double* inverse_ptr;
     inverse_ptr = inverse;
 
