ddc7820bbc6f090962b77929dc910cef72fd610a
Ollie M. <olliemiller500@protonmail.com>
2026-05-15 11:11:39 +0100

    Update inversion.hpp

--- inversion.hpp
+++ inversion.hpp
@@ -3,6 +3,7 @@
 
 void gaussian_elimination(double* arr_ptr, double* inv_arr_return, int n, double epsilon=1e-6);
 void print_matrix(double* arr, int n, std::string title="", int setw_size=10);
+double determinant(double* arr_ptr, int n, double epsilon=1e-9);
 
 #endif
 
